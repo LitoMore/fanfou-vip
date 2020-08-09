07be9340c957bcdad1427a1174e4c71c03e8570e
LitoMore <litomore@gmail.com>
2020-08-09 19:46:54 +0800

    Add action buttons

--- src/components/status.tsx
+++ src/components/status.tsx
@@ -1,6 +1,8 @@
 import React, {FunctionComponent} from 'react';
 import styled from 'styled-components';
+import {queries} from '../global-style';
 import {Status as StatusType} from '../types/fanfou';
+import favGray from '../assets/fav-gray.svg';
 
 type StatusProps = {
 	className?: string;
@@ -9,17 +11,39 @@ type StatusProps = {
 
 const StatusDiv: FunctionComponent<StatusProps> = ({className, status}) => (
 	<div className={className}>
-		<Slot>
-			<Avatar src={status.user.profile_image_url}/>
-		</Slot>
-		<Slot flex="1" direction="column">
-			<Name>{status.user.name}</Name>
-			<Text>{status.text}</Text>
-		</Slot>
+		<Row>
+			<Slot>
+				<Avatar src={status.user.profile_image_url}/>
+			</Slot>
+			<Slot flex="1" direction="column">
+				<Name>{status.user.name}</Name>
+				<Text>{status.text}</Text>
+			</Slot>
+			<Slot>
+				<Actions for="web">
+					<Fav/>
+					<Fav/>
+					<Fav/>
+				</Actions>
+			</Slot>
+		</Row>
+		<Row>
+			<Actions for="mobile">
+				<Fav/>
+				<Fav/>
+				<Fav/>
+			</Actions>
+		</Row>
 	</div>
 );
 
 const StyledStatus = styled(StatusDiv)`
+	display: flex;
+	flex-direction: column;
+`;
+
+const Row = styled.div`
+	position: relative;
 	display: flex;
 `;
 
@@ -49,6 +73,43 @@ const Text = styled.div`
 	color: var(--textBlack);
 	font-size: 14px;
 	line-height: 1.3125;
+`;
+
+const Actions = styled.div<{
+	for: 'web' | 'mobile';
+}>`
+	display: none;
+	justify-content: space-around;
+	align-items: center;
+
+	@media ${queries.web} {
+		${StyledStatus}:hover & {
+			position: absolute;
+			flex-direction: column;
+			top: 0;
+			right: 0;
+			${props => props.for === 'web' ? 'display: flex;' : ''}
+		}
+	}
+
+	@media ${queries.mobile} {
+		width: 100px;
+		align-self: right;
+		margin-left: auto;
+		height: 24px;
+		${props => props.for === 'mobile' ? 'display: flex;' : ''}
+	}
+`;
+
+const Icon = styled.div`
+	width: 20px;
+	height: 20px;
+	background-position: center center;
+	background-size: contain;
+`;
+
+const Fav = styled(Icon)`
+	background-image: url(${favGray});
 `;
 
 export default StyledStatus;
--- src/global-style.ts
+++ src/global-style.ts
@@ -5,5 +5,13 @@ export const GlobalStyle = createGlobalStyle`
 		--fanfouBlue: #0cf;
 		--linkBlue: #06c;
 		--textBlack: #222;
+
+		--mobileQuery: ;
 	}
 `;
+
+export const queries = {
+	mobile: '(max-width: 767px)',
+	pad: '(max-width: 1024px)',
+	web: '(min-width: 1024px)'
+};
